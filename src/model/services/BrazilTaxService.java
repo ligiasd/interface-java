bb4fe7744e4b6ac2c461ccf3e118cb4c1110ef81
Ligiane Alves Souza Domingos <ligiane.alves.souza@gmail.com>
2022-02-22 00:29:07 -0300

    add app package

--- src/model/services/BrazilTaxService.java
+++ src/model/services/BrazilTaxService.java
@@ -3,7 +3,7 @@ package model.services;
 public class BrazilTaxService {
 
     public double tax(double amount) {
-        if (amount >= 100.00) {
+        if (amount <= 100.0) {
             return amount * 0.2;
         } else {
             return amount * 0.15;
